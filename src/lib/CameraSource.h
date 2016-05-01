cb8c7413115ee40a9c50385d0c37a3310dddd725
David Avedissian <git@davedissian.com>
2016-05-01 18:19:23 +0100

    Distance coefficients should be doubles

--- src/lib/CameraSource.h
+++ src/lib/CameraSource.h
@@ -30,12 +30,12 @@ public:
     int getHeight() const { return mHeight; }
     GLuint getTexture(Eye e) const { return mTexture[e]; }
     cv::Mat& getCaneraMatrix() { return mCameraMatrix; }
-    std::vector<float>& getDistCoeffs() { return mDistCoeffs; }
+    std::vector<double>& getDistCoeffs() { return mDistCoeffs; }
 
 protected:
     int mWidth, mHeight;
     GLuint mTexture[2];
     cv::Mat mCameraMatrix;
-    std::vector<float> mDistCoeffs;
+    std::vector<double> mDistCoeffs;
 
 };
